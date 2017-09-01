6ec053eb16a554f75cb88de941ba866b7ab7392d
developermypos <yavor.petrov@icard.com>
2017-09-01 10:51:18 +0300

    Initial commit

---
+++ myPOS-SDK-Demo/myPOSDemoApp/Form1.Designer.cs
@@ -0,0 +1,352 @@
+﻿namespace myPOSDemoApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cmbComPorts = new System.Windows.Forms.ComboBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnPurchase = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.btnAbort = new System.Windows.Forms.Button();
+            this.btnGetStatus = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.cmbCurrency = new System.Windows.Forms.ComboBox();
+            this.btnRefund = new System.Windows.Forms.Button();
+            this.btnReversal = new System.Windows.Forms.Button();
+            this.btnActivate = new System.Windows.Forms.Button();
+            this.btnDeactivate = new System.Windows.Forms.Button();
+            this.btnReprint = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbReceiptMode = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtReference = new System.Windows.Forms.TextBox();
+            this.txtPrintData = new System.Windows.Forms.TextBox();
+            this.btnPrintExternal = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmbComPorts
+            // 
+            this.cmbComPorts.FormattingEnabled = true;
+            this.cmbComPorts.Location = new System.Drawing.Point(74, 10);
+            this.cmbComPorts.Name = "cmbComPorts";
+            this.cmbComPorts.Size = new System.Drawing.Size(176, 21);
+            this.cmbComPorts.TabIndex = 0;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(256, 8);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 1;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(337, 8);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnPurchase
+            // 
+            this.btnPurchase.Location = new System.Drawing.Point(24, 244);
+            this.btnPurchase.Name = "btnPurchase";
+            this.btnPurchase.Size = new System.Drawing.Size(75, 23);
+            this.btnPurchase.TabIndex = 3;
+            this.btnPurchase.Text = "Purchase";
+            this.btnPurchase.UseVisualStyleBackColor = true;
+            this.btnPurchase.Click += new System.EventHandler(this.btnPurchase_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLog.BackColor = System.Drawing.SystemColors.Window;
+            this.txtLog.Location = new System.Drawing.Point(12, 415);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtLog.Size = new System.Drawing.Size(895, 356);
+            this.txtLog.TabIndex = 4;
+            // 
+            // btnAbort
+            // 
+            this.btnAbort.Location = new System.Drawing.Point(24, 302);
+            this.btnAbort.Name = "btnAbort";
+            this.btnAbort.Size = new System.Drawing.Size(75, 23);
+            this.btnAbort.TabIndex = 6;
+            this.btnAbort.Text = "Abort";
+            this.btnAbort.UseVisualStyleBackColor = true;
+            this.btnAbort.Click += new System.EventHandler(this.btnAbort_Click);
+            // 
+            // btnGetStatus
+            // 
+            this.btnGetStatus.Location = new System.Drawing.Point(24, 56);
+            this.btnGetStatus.Name = "btnGetStatus";
+            this.btnGetStatus.Size = new System.Drawing.Size(75, 23);
+            this.btnGetStatus.TabIndex = 8;
+            this.btnGetStatus.Text = "Get Status";
+            this.btnGetStatus.UseVisualStyleBackColor = true;
+            this.btnGetStatus.Click += new System.EventHandler(this.btnGetStatus_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(114, 56);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 9;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "COM port:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Amount";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Currency";
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Location = new System.Drawing.Point(114, 133);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(164, 20);
+            this.txtAmount.TabIndex = 13;
+            // 
+            // cmbCurrency
+            // 
+            this.cmbCurrency.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbCurrency.FormattingEnabled = true;
+            this.cmbCurrency.Location = new System.Drawing.Point(114, 163);
+            this.cmbCurrency.Name = "cmbCurrency";
+            this.cmbCurrency.Size = new System.Drawing.Size(164, 21);
+            this.cmbCurrency.TabIndex = 14;
+            // 
+            // btnRefund
+            // 
+            this.btnRefund.Location = new System.Drawing.Point(114, 244);
+            this.btnRefund.Name = "btnRefund";
+            this.btnRefund.Size = new System.Drawing.Size(75, 23);
+            this.btnRefund.TabIndex = 15;
+            this.btnRefund.Text = "Refund";
+            this.btnRefund.UseVisualStyleBackColor = true;
+            this.btnRefund.Click += new System.EventHandler(this.btnRefund_Click);
+            // 
+            // btnReversal
+            // 
+            this.btnReversal.Location = new System.Drawing.Point(203, 244);
+            this.btnReversal.Name = "btnReversal";
+            this.btnReversal.Size = new System.Drawing.Size(75, 23);
+            this.btnReversal.TabIndex = 16;
+            this.btnReversal.Text = "Reversal";
+            this.btnReversal.UseVisualStyleBackColor = true;
+            this.btnReversal.Click += new System.EventHandler(this.btnReversal_Click);
+            // 
+            // btnActivate
+            // 
+            this.btnActivate.Location = new System.Drawing.Point(203, 56);
+            this.btnActivate.Name = "btnActivate";
+            this.btnActivate.Size = new System.Drawing.Size(75, 23);
+            this.btnActivate.TabIndex = 17;
+            this.btnActivate.Text = "Activate";
+            this.btnActivate.UseVisualStyleBackColor = true;
+            this.btnActivate.Click += new System.EventHandler(this.btnActivate_Click);
+            // 
+            // btnDeactivate
+            // 
+            this.btnDeactivate.Location = new System.Drawing.Point(294, 56);
+            this.btnDeactivate.Name = "btnDeactivate";
+            this.btnDeactivate.Size = new System.Drawing.Size(75, 23);
+            this.btnDeactivate.TabIndex = 17;
+            this.btnDeactivate.Text = "Deactivate";
+            this.btnDeactivate.UseVisualStyleBackColor = true;
+            this.btnDeactivate.Click += new System.EventHandler(this.btnDeactivate_Click);
+            // 
+            // btnReprint
+            // 
+            this.btnReprint.Location = new System.Drawing.Point(294, 244);
+            this.btnReprint.Name = "btnReprint";
+            this.btnReprint.Size = new System.Drawing.Size(117, 23);
+            this.btnReprint.TabIndex = 18;
+            this.btnReprint.Text = "Reprint receipt";
+            this.btnReprint.UseVisualStyleBackColor = true;
+            this.btnReprint.Click += new System.EventHandler(this.btnReprint_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Receipt mode";
+            // 
+            // cmbReceiptMode
+            // 
+            this.cmbReceiptMode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbReceiptMode.FormattingEnabled = true;
+            this.cmbReceiptMode.Location = new System.Drawing.Point(114, 101);
+            this.cmbReceiptMode.Name = "cmbReceiptMode";
+            this.cmbReceiptMode.Size = new System.Drawing.Size(164, 21);
+            this.cmbReceiptMode.TabIndex = 14;
+            this.cmbReceiptMode.SelectedIndexChanged += new System.EventHandler(this.cmbCurrency_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(21, 196);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 13);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Reference";
+            // 
+            // txtReference
+            // 
+            this.txtReference.Location = new System.Drawing.Point(114, 193);
+            this.txtReference.Name = "txtReference";
+            this.txtReference.Size = new System.Drawing.Size(164, 20);
+            this.txtReference.TabIndex = 22;
+            // 
+            // txtPrintData
+            // 
+            this.txtPrintData.Location = new System.Drawing.Point(512, 13);
+            this.txtPrintData.Multiline = true;
+            this.txtPrintData.Name = "txtPrintData";
+            this.txtPrintData.Size = new System.Drawing.Size(341, 254);
+            this.txtPrintData.TabIndex = 23;
+            this.txtPrintData.Text = resources.GetString("txtPrintData.Text");
+            // 
+            // btnPrintExternal
+            // 
+            this.btnPrintExternal.Location = new System.Drawing.Point(512, 302);
+            this.btnPrintExternal.Name = "btnPrintExternal";
+            this.btnPrintExternal.Size = new System.Drawing.Size(341, 23);
+            this.btnPrintExternal.TabIndex = 24;
+            this.btnPrintExternal.Text = "Print external data";
+            this.btnPrintExternal.UseVisualStyleBackColor = true;
+            this.btnPrintExternal.Click += new System.EventHandler(this.btnPrintExternal_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(919, 783);
+            this.Controls.Add(this.btnPrintExternal);
+            this.Controls.Add(this.txtPrintData);
+            this.Controls.Add(this.txtReference);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnReprint);
+            this.Controls.Add(this.btnDeactivate);
+            this.Controls.Add(this.btnActivate);
+            this.Controls.Add(this.btnReversal);
+            this.Controls.Add(this.btnRefund);
+            this.Controls.Add(this.cmbReceiptMode);
+            this.Controls.Add(this.cmbCurrency);
+            this.Controls.Add(this.txtAmount);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnGetStatus);
+            this.Controls.Add(this.btnAbort);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnPurchase);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.cmbComPorts);
+            this.Name = "Form1";
+            this.Text = "Test Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbComPorts;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnPurchase;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Button btnAbort;
+        private System.Windows.Forms.Button btnGetStatus;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.ComboBox cmbCurrency;
+        private System.Windows.Forms.Button btnRefund;
+        private System.Windows.Forms.Button btnReversal;
+        private System.Windows.Forms.Button btnActivate;
+        private System.Windows.Forms.Button btnDeactivate;
+        private System.Windows.Forms.Button btnReprint;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbReceiptMode;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtReference;
+        private System.Windows.Forms.TextBox txtPrintData;
+        private System.Windows.Forms.Button btnPrintExternal;
+    }
+}
+
---
+++ myPOS-SDK-Demo/myPOSDemoApp/Form1.cs
@@ -0,0 +1,268 @@
+﻿using myPOS;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO.Ports;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace myPOSDemoApp
+{
+    public partial class Form1 : Form
+    {
+        double Amount;
+        Currencies cur;
+        Version version = Assembly.GetEntryAssembly().GetName().Version;
+
+        myPOSTerminal t = new myPOSTerminal();
+        public Form1()
+        {
+            InitializeComponent();
+            
+            t.ProcessingFinished += ProcessResult;
+            t.Log += AddLog;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RefreshComPorts();
+            cmbCurrency.DataSource = Enum.GetValues(typeof(Currencies));
+            cmbCurrency.SelectedItem = Currencies.EUR;
+            cmbReceiptMode.DataSource = Enum.GetValues(typeof(ReceiptMode));
+            cmbReceiptMode.SelectedItem = ReceiptMode.NotConfugred;
+
+            this.Text = "TestApp Version " + version.ToString();
+            AddLog("TestApp Version: " + version.ToString());
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            RefreshComPorts();
+        }
+
+        void RefreshComPorts()
+        {
+            cmbComPorts.Items.Clear();
+            cmbComPorts.Items.AddRange(SerialPort.GetPortNames());
+        }
+
+        protected void ProcessResult(ProcessingResult r)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("Processing \"{0}\" finished\r\n", r.Method.ToString());
+            sb.AppendFormat("Status: \"{0}\"\r\n", r.Status.ToString());
+
+            if (r.TranData != null)
+            {
+                sb.AppendFormat("\r\nTransaction data:\r\n");
+                sb.AppendFormat("Type: {0}\r\n", r.TranData.Type);
+                sb.AppendFormat("Amount: {0}\r\n", r.TranData.Amount);
+                sb.AppendFormat("Currency: {0}\r\n", r.TranData.Currency.ToString());
+                sb.AppendFormat("Approval: {0}\r\n", r.TranData.Approval);
+                sb.AppendFormat("Auth code: {0}\r\n", r.TranData.AuthCode);
+                sb.AppendFormat("RRN: {0}\r\n", r.TranData.RRN);
+                sb.AppendFormat("Date: {0}\r\n", r.TranData.TransactionDate.ToString("dd.MM.yyyy"));
+                sb.AppendFormat("Time: {0}\r\n", r.TranData.TransactionDate.ToString("HH:mm:ss"));
+                sb.AppendFormat("Terminal ID: {0}\r\n", r.TranData.TerminalID);
+                sb.AppendFormat("Merchant ID: {0}\r\n", r.TranData.MerchantID);
+                sb.AppendFormat("Merchant Name: {0}\r\n", r.TranData.MerchantName);
+                sb.AppendFormat("Merchant Address Line 1: {0}\r\n", r.TranData.MerchantAddressLine1);
+                sb.AppendFormat("Merchant Address Line 2: {0}\r\n", r.TranData.MerchantAddressLine2);
+                sb.AppendFormat("PAN Masked: {0}\r\n", r.TranData.PANMasked);
+                sb.AppendFormat("Emboss Name: {0}\r\n", r.TranData.EmbossName);
+                sb.AppendFormat("AID: {0}\r\n", r.TranData.AID);
+                sb.AppendFormat("AID Name: {0}\r\n", r.TranData.AIDName);
+                sb.AppendFormat("STAN: {0}\r\n", r.TranData.Stan);
+                sb.AppendFormat("Signature Required: {0}\r\n", r.TranData.SignatureRequired ? "Yes" : "No");
+            }
+
+            AddLog(sb.ToString());
+            MessageBox.Show(sb.ToString());
+        }
+
+        delegate void AddLogDelegate(string msg);
+        protected void AddLog(string msg)
+        {
+            if (txtLog.InvokeRequired)
+            {
+                object[] p = new object[1];
+                p[0] = msg;
+                txtLog.Invoke(new AddLogDelegate(AddLog), p);
+                return;
+            }
+
+            try
+            {
+                txtLog.AppendText(msg + "\r\n");
+            }
+            catch { }
+        }
+
+        private bool ParseAmount()
+        {
+            string amt = txtAmount.Text;
+            amt = amt.Replace(",", ".");
+            return Double.TryParse(amt, NumberStyles.Number, CultureInfo.InvariantCulture, out Amount);
+        }
+
+        private bool ParseCurrency()
+        {
+            cur = (Currencies)cmbCurrency.SelectedItem;
+            return true;
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (cmbComPorts.SelectedIndex < 0) return;
+
+            t.Initialize((string)cmbComPorts.SelectedItem);
+        }
+
+        private void btnAbort_Click(object sender, EventArgs e)
+        {
+            t.AbortOperation();
+        }
+
+        private void btnGetStatus_Click(object sender, EventArgs e)
+        {
+            RequestResult r = t.GetStatus();
+            switch (r)
+            {
+                case RequestResult.Busy:
+                case RequestResult.InvalidParams:
+                case RequestResult.NotInitialized:
+                    MessageBox.Show("RequestResult: " + r.ToString());
+                    break;
+                default: break;
+            }
+        }
+
+        private void btnPurchase_Click(object sender, EventArgs e)
+        {
+            if (!ParseAmount() || !ParseCurrency())
+            {
+                MessageBox.Show("Invalid input");
+            }
+            else
+            {
+                RequestResult r = t.Purchase(Amount, cur, txtReference.Text);
+                switch (r)
+                {
+                    case RequestResult.Busy:
+                    case RequestResult.InvalidParams:
+                    case RequestResult.NotInitialized:
+                        MessageBox.Show("RequestResult: " + r.ToString());
+                        break;
+                    default: break;
+                }
+            }
+        }
+
+        private void btnRefund_Click(object sender, EventArgs e)
+        {
+            if (!ParseAmount() || !ParseCurrency())
+            {
+                MessageBox.Show("Invalid input");
+            }
+            else
+            {
+                RequestResult r = t.Refund(Amount, cur, txtReference.Text);
+                switch (r)
+                {
+                    case RequestResult.Busy:
+                    case RequestResult.InvalidParams:
+                    case RequestResult.NotInitialized:
+                        MessageBox.Show("RequestResult: " + r.ToString());
+                        break;
+                    default: break;
+                }
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            RequestResult r = t.Update();
+            switch (r)
+            {
+                case RequestResult.Busy:
+                case RequestResult.InvalidParams:
+                case RequestResult.NotInitialized:
+                    MessageBox.Show("RequestResult: " + r.ToString());
+                    break;
+                default: break;
+            }
+        }
+
+        private void btnReversal_Click(object sender, EventArgs e)
+        {
+            RequestResult r = t.Reversal(txtReference.Text);
+            switch (r)
+            {
+                case RequestResult.Busy:
+                case RequestResult.InvalidParams:
+                case RequestResult.NotInitialized:
+                    MessageBox.Show("RequestResult: " + r.ToString());
+                    break;
+                default: break;
+            }
+        }
+
+        private void btnActivate_Click(object sender, EventArgs e)
+        {
+            RequestResult r = t.Activate();
+            switch (r)
+            {
+                case RequestResult.Busy:
+                case RequestResult.InvalidParams:
+                case RequestResult.NotInitialized:
+                    MessageBox.Show("RequestResult: " + r.ToString());
+                    break;
+                default: break;
+            }
+        }
+
+        private void btnDeactivate_Click(object sender, EventArgs e)
+        {
+            RequestResult r = t.Deactivate();
+            switch (r)
+            {
+                case RequestResult.Busy:
+                case RequestResult.InvalidParams:
+                case RequestResult.NotInitialized:
+                    MessageBox.Show("RequestResult: " + r.ToString());
+                    break;
+                default: break;
+            }
+        }
+
+        private void btnReprint_Click(object sender, EventArgs e)
+        {
+            RequestResult r = t.ReprintReceipt();
+            switch (r)
+            {
+                case RequestResult.Busy:
+                case RequestResult.InvalidParams:
+                case RequestResult.NotInitialized:
+                    MessageBox.Show("RequestResult: " + r.ToString());
+                    break;
+                default: break;
+            }
+        }
+
+        private void cmbCurrency_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            t.SetReceiptMode((ReceiptMode)cmbReceiptMode.SelectedItem);
+        }
+
+        private void btnPrintExternal_Click(object sender, EventArgs e)
+        {
+            t.PrintExternal(txtPrintData.Text);
+        }
+    }
+}
